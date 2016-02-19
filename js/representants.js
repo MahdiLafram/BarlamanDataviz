57cab5afbadae74864eaf546e73c3894ee01c818
Mahdi Lafram <elmahdi.lafram@gmail.com>
2016-02-19 23:14:04 +0000

    few experiments

--- js/representants.js
+++ js/representants.js
@@ -1,7 +1,7 @@
     
 
   var data = [107, 60, 52, 47, 39, 32, 23, 18, 4, 13];
-  var label = ["PJD", "PAM", "stuff"];
+  var name = ['PJD', 'PAM', 'stuff', 'stuff', 'stuff', 'stuff', 'stuff'];
   var r = 90;
 
   var width = 420;
@@ -12,7 +12,10 @@
                   .attr("width", width)
                   .attr("height", height);
 
-  var color = d3.scale.category20();
+  var color = d3.scale.ordinal()
+                      .domain(["Lorem ipsum", "dolor sit", "amet", "consectetur", "adipisicing", "elit", "sed", "do", "eiusmod", "tempor", "incididunt"])
+                      .range(["#98abc5", "#8a89a6", "#7b6888", "#6b486b", "#a05d56", "#d0743c", "#ff8c00"]);
+
 
   var group = canvas.append("g")
                     .attr("transform", "translate(220, 180)");
@@ -37,5 +40,4 @@
   arcs.append("text")
   .attr("transform", function (d) { return "translate(" + arc.centroid(d) + ")"; })
   .attr("text-anchor", "middle")
-  .attr("color", "white")
-  .text( function (d) {return d.data; });
+  .text( function (d) { return d.data; });
